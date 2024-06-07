12996a27e850eb26c9872f00846177e20ddc25b9
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-07 13:11:54 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,18 @@
+var a=10;
+document.write(a," ");
+let b=20;
+document.write(b," ");
+const c=30;
+document.write(c," ");
+function add(){
+    var d=10
+document.write(d+60," ");
+}
+
+add()
+var e=25
+function add1(){
+
+}
+document.write(e," ");
+document.write(e+25);
